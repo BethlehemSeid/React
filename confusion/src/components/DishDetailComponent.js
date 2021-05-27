f07af340e11a4bdf97a318dc1cbf8b218b7b24eb
Bethlehem Seid <betty53.msi@gmail.com>
2021-05-27 09:52:32 -0400

    Fetch handling errors

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -3,7 +3,7 @@ import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem
 import {BrowserRouter, Link} from 'react-router-dom';
 import {LocalForm, Errors, Control} from 'react-redux-form';
 import {Loading} from './LoadingComponent';
-import {addComment} from '../redux/ActionCreators';
+import {baseUrl} from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -136,7 +136,7 @@ function RenderDish({dish}) {
     if(dish != null){
         return(
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
